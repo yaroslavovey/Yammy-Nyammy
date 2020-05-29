9b5d7ba2bb9f58edd892704f81583d3272c657ef
ph00per <yaroslavovey@gmail.com>
2020-05-30 01:28:51 +0300

    Modules Updated

--- app/src/main/java/com/phooper/yammynyammy/di/Modules.kt
+++ app/src/main/java/com/phooper/yammynyammy/di/Modules.kt
@@ -4,6 +4,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.phooper.yammynyammy.R
+import com.phooper.yammynyammy.data.repositories.UserRepository
 import com.phooper.yammynyammy.viewmodels.LoginViewModel
 import com.phooper.yammynyammy.viewmodels.MainContainerViewModel
 import com.phooper.yammynyammy.viewmodels.SplashScreenViewModel
@@ -11,14 +12,9 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val viewModelModule = module {
-    viewModel { MainContainerViewModel() }
-    viewModel { SplashScreenViewModel() }
-    viewModel { LoginViewModel() }
-}
-
 val firebaseModule = module {
     single { FirebaseAuth.getInstance() }
+    //Google sign in options
     single {
         GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(androidContext().resources.getString(R.string.default_web_client_id))
@@ -29,4 +25,17 @@ val firebaseModule = module {
         GoogleSignIn.getClient(androidContext(), get())
     }
 }
+
+val repositoryModule = module {
+    single {
+        UserRepository(get())
+    }
+}
+
+val viewModelModule = module {
+    viewModel { MainContainerViewModel() }
+    viewModel { SplashScreenViewModel(get()) }
+    viewModel { LoginViewModel() }
+}
+
 
