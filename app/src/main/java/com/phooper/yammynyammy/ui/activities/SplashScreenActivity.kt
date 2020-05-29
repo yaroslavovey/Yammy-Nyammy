05aeacd1661cda851a7b976520e389faef19b026
ph00per <yaroslavovey@gmail.com>
2020-05-30 01:30:40 +0300

    Added current user login check on splash activity

--- app/src/main/java/com/phooper/yammynyammy/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/phooper/yammynyammy/ui/activities/SplashScreenActivity.kt
@@ -3,16 +3,32 @@ package com.phooper.yammynyammy.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.phooper.yammynyammy.viewmodels.SplashScreenViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreenViewModel by viewModel<SplashScreenViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        splashScreenViewModel.isSignedIn.observe(this, Observer {
+            when (it) {
+                true -> {
+                    startActivity(Intent(this, MainContainerActivity::class.java))
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+                false -> {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                    finish()
+                }
+            }
+        })
 
 
-        startActivity(Intent(this, LoginActivity::class.java))
-        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-        finish()
     }
 }
--- app/src/main/java/com/phooper/yammynyammy/viewmodels/SplashScreenViewModel.kt
+++ app/src/main/java/com/phooper/yammynyammy/viewmodels/SplashScreenViewModel.kt
@@ -1,7 +1,27 @@
 package com.phooper.yammynyammy.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.phooper.yammynyammy.data.repositories.UserRepository
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class SplashScreenViewModel : ViewModel(){
+class SplashScreenViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    val isSignedIn = MutableLiveData<Boolean>()
+
+    init {
+        checkUser()
+    }
+
+    private fun checkUser() {
+        viewModelScope.launch {
+            isSignedIn.value = userRepository
+                .getCurrentUser() != null
+        }
+
+    }
 
 }
+
