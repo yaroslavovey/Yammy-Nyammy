c169adb66f7836d2475892aac114e9ae8b47b4b7
ph00per <yaroslavovey@gmail.com>
2020-05-30 02:07:49 +0300

    Code formatted, useless imports removed.

--- app/src/main/java/com/phooper/yammynyammy/utils/Utils.kt
+++ app/src/main/java/com/phooper/yammynyammy/utils/Utils.kt
@@ -1,14 +1,12 @@
 package com.phooper.yammynyammy.utils
 
-import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 fun setHideErrorOnTextChangedListener(vararg layoutsAndEdiTextPairs: Pair<TextInputLayout, TextInputEditText>) {
+
     layoutsAndEdiTextPairs.forEach {
         it.second.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
