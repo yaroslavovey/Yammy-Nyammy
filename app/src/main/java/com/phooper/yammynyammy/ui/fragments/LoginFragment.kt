1e971b56459de963b8ca322b8d3e2b0763302e28
ph00per <yaroslavovey@gmail.com>
2020-06-01 20:49:44 +0300

    added email | google auth

--- app/src/main/java/com/phooper/yammynyammy/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/phooper/yammynyammy/ui/fragments/LoginFragment.kt
@@ -3,28 +3,24 @@ package com.phooper.yammynyammy.ui.fragments
 import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import com.phooper.yammynyammy.R
-import com.phooper.yammynyammy.utils.setHideErrorOnTextChangedListener
+import com.phooper.yammynyammy.utils.Constants.Companion.G_AUTH_REQUEST_CODE
+import com.phooper.yammynyammy.utils.hideKeyboard
+import com.phooper.yammynyammy.utils.setHideLayoutErrorOnTextChangedListener
+import com.phooper.yammynyammy.viewmodels.LoginViewModel
 import kotlinx.android.synthetic.main.fragment_login.*
 import org.koin.android.ext.android.inject
-import timber.log.Timber
 
 class LoginFragment : BaseFragment() {
+    private lateinit var navController: NavController
     override val layoutRes = R.layout.fragment_login
 
-    private val REQUEST_CODE = 3232
-
-    private lateinit var navController: NavController
-
-    private val googleSignInClient: GoogleSignInClient by inject()
-    private val firebaseAuth: FirebaseAuth by inject()
+    private val viewModel by activityViewModels<LoginViewModel>()
+    private val googleSignInClient by inject<GoogleSignInClient>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -33,34 +29,23 @@ class LoginFragment : BaseFragment() {
     }
 
     private fun initViews() {
-        setHideErrorOnTextChangedListener(
-            Pair(password_input_layout, password_input),
-            Pair(email_input_layout, email_input)
-        )
+        password_input.setHideLayoutErrorOnTextChangedListener(password_input_layout)
+        email_input.setHideLayoutErrorOnTextChangedListener(email_input_layout)
+
         password_input.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                if (areInputsFilled()) {
-                    authWithEmailAndPassword(
-                        email_input.text.toString(),
-                        password_input.text.toString()
-                    )
-                }
+                signInViaEmail()
                 return@setOnEditorActionListener true
             }
             false
         }
 
         login_btn.setOnClickListener {
-            if (areInputsFilled()) {
-                authWithEmailAndPassword(
-                    email_input.text.toString(),
-                    password_input.text.toString()
-                )
-            }
+            signInViaEmail()
         }
 
         login_via_google.setOnClickListener {
-            startActivityForResult(googleSignInClient.signInIntent, REQUEST_CODE)
+            startActivityForResult(googleSignInClient.signInIntent, G_AUTH_REQUEST_CODE)
         }
 
         register_btn.setOnClickListener {
@@ -68,62 +53,34 @@ class LoginFragment : BaseFragment() {
         }
     }
 
-    private fun areInputsFilled(): Boolean {
-        when {
-            (email_input.text.toString().isEmpty() && password_input.text.toString()
-                .isEmpty()) -> {
-                password_input_layout.error = getString(R.string.fill_password)
-                email_input_layout.error = getString(R.string.fill_email)
-                return false
-            }
-            (email_input.text.toString().isEmpty()) -> {
-                email_input_layout.error = getString(R.string.fill_email)
-                return false
-            }
-            (password_input.text.toString().isEmpty()) -> {
-                password_input_layout.error = getString(R.string.fill_password)
-                return false
-            }
-            else -> {
-                return true
-            }
+    private fun signInViaEmail() {
+        if (areInputsEmpty()) {
+            showFillFieldsError()
+            return
         }
+        hideKeyboard()
+        viewModel.handleSignInViaEmail(
+            email_input.text.toString(),
+            password_input.text.toString()
+        )
     }
 
-    private fun authWithEmailAndPassword(email: String, password: String) {
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    showMessage("Успех!", main_constraint_layout)
-                    Timber.d("Success")
-                } else {
-                    showMessage("Ошибка!", main_constraint_layout)
-                    Timber.d("Fail")
-                }
-            }
-    }
+    private fun areInputsEmpty() =
+        password_input.text.toString().isEmpty() ||
+                email_input.text.toString().isEmpty()
 
-    private fun authWithGoogle(signInAccount: GoogleSignInAccount?) {
-        val credential =
-            GoogleAuthProvider.getCredential(signInAccount?.idToken, null)
-        firebaseAuth.signInWithCredential(credential)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Timber.d("Auth success")
-                    Timber.d(signInAccount?.displayName)
-                } else {
-                    Timber.d("Some problems")
-                }
-            }
+    private fun showFillFieldsError() {
+        email_input_layout.error =
+            if (email_input.text.toString().isEmpty()) getString(R.string.fill_email) else null
+
+        password_input_layout.error =
+            if (password_input.text.toString()
+                    .isEmpty()
+            ) getString(R.string.fill_password) else null
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE) {
-            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if (result.isSuccess) {
-                authWithGoogle(result.signInAccount)
-            }
-        }
+        viewModel.handleOnActivityResult(requestCode, data)
     }
 }
