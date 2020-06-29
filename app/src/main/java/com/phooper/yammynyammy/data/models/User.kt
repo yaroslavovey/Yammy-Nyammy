bda1d90afb9cccfb99dd0bb76a82e990abe63fcd
ph00per <yaroslavovey@gmail.com>
2020-06-29 11:05:35 +0300

    removed addresses as list in user

--- app/src/main/java/com/phooper/yammynyammy/data/models/User.kt
+++ app/src/main/java/com/phooper/yammynyammy/data/models/User.kt
@@ -2,6 +2,5 @@ package com.phooper.yammynyammy.data.models
 
 data class User(
     val phoneNum: String = "",
-    val name: String = "",
-    val addresses: List<Address>? = null
+    val name: String = ""
 )
